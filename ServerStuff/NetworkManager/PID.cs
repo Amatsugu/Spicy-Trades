1b8792da196a902fc6445d843abfe019719abd8f
Ryan Ward <rayaman99@gmail.com>
2018-03-04 00:11:36 -0500

    Added new client stuff
    basic class stuff

---
+++ ServerStuff/NetworkManager/PID.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkManager
+{
+    class PID
+    {
+        private string id;
+        private string name;
+        private bool isFriend;
+
+        public PID(string id,string name,bool isFriend)
+        {
+            this.id = id;
+            this.name = name;
+            this.isFriend = isFriend;
+        }
+        public string GetID()
+        {
+            return id;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public bool isAFriend()
+        {
+            return isFriend;
+        }
+        public void isAFriend(bool b)
+        {
+            isFriend = b;
+        }
+    }
+}
---
+++ ServerStuff/NetworkManager/Room.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkManager
+{
+    class Room
+    {
+        private int MAX_MEMBERS = 4; //Max members that can be on a server
+        private string roomID;
+        private string roomPass;
+        private PID[] members;
+        private bool host;
+        private Client client;
+        private bool isReady;
+        private bool roomJoined=false;
+
+        public Room(string roomID, string roomPass, bool host,Client client)
+        {
+            this.roomID = roomID;
+            this.roomPass = roomPass;
+            this.host = host;
+            this.client = client;
+            members = new PID[MAX_MEMBERS];
+        }
+        public void Join()
+        {
+            roomJoined = true;
+        }
+        public bool isHost()
+        {
+            return host;
+        }
+        public void AddMember(PID member)
+        {
+            // TODO Add member to the room, change local data and send command to the server
+        }
+        public bool Kick(PID member)
+        {
+            // TODO Kick Member from the room... Should this be host only or anyone...
+            return false;
+        }
+        public void LeaveRoom()
+        {
+            // TODO Leave the room
+        }
+        public void SetReady(bool b)
+        {
+            isReady = b;
+        }
+        public void SendChat(string message)
+        {
+            // TODO add chatting support in rooms
+        }
+    }
+}
