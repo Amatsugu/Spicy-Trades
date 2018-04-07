e1650b95ee36efca32cf2a39fa3530535e6aaf54
Khamraj Rohit <khamraj@luminousvector.com>
2018-04-07 19:12:21 -0400

    Bug Fixes and Events

--- SpicyTrades/Assets/Script/SettlementEvents/SettlementEvent.cs
+++ SpicyTrades/Assets/Script/SettlementEvents/SettlementEvent.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Data Providers/Event")]
 public class SettlementEvent : ScriptableObject
 {
 	public string Description { get; set; }
 	[Range(0, 100)]
 	public int Chance;
+
+	public List<TradePackage> resourceDemands = new List<TradePackage>();
+	public int duration;
 
 }
