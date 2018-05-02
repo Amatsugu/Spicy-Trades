46525cf6fbc98d05e53467dde2a9956a9659936f
TheDarkVoid <khamraj@luminousvector.com>
2018-05-02 18:03:21 -0400

    Query Optimizations

--- SpicyTrades/Assets/Script/Trading/InventoryItem.cs
+++ SpicyTrades/Assets/Script/Trading/InventoryItem.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class InventoryItem
 {
+	public ResourceTileInfo ActualResource
+	{ get
+		{
+			if (_actualResource == null)
+				return _actualResource = GameMaster.Registry.resourceList.GetResourceList().First(res => Resource.Match(res));
+			else
+				return _actualResource;
+		}
+	}
 	public ResourceIdentifier Resource { get; set; }
 	public float Cost { get; set; }
+
+	public ResourceTileInfo _actualResource;
 }
 
