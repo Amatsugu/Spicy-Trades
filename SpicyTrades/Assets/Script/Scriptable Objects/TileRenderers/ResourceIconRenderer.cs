ae5440e1bee1baf1cb4339c1808618d4c4afd20d
Khamraj Rohit <khamraj@luminousvector.com>
2018-05-04 09:00:02 -0400

    Rendering Upgrades

---
+++ SpicyTrades/Assets/Script/Scriptable Objects/TileRenderers/ResourceIconRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tile Renderer/Resource Icon")]
+public class ResourceIconRenderer : TileRenderer
+{
+	public override void PostRender(Tile tile, object renderData)
+	{
+	}
+
+	public override void RenderInit(Tile tile)
+	{
+		if (tile.GetType() != typeof(ResourceTile))
+			return;
+		var sprite = new GameObject();
+		sprite.transform.parent = tile.ThisTransform;
+		sprite.transform.localPosition = Vector3.zero;
+		sprite.transform.localScale = new Vector3(.8f, .8f, .8f);
+		var sr = sprite.AddComponent<SpriteRenderer>();
+		sr.sortingOrder = 3;
+		sr.sprite = (tile.tileInfo as ResourceTileInfo).icon;
+	}
+}
---
+++ SpicyTrades/Assets/Script/Scriptable Objects/TileRenderers/UnTilledRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tile Renderer/UnTiled")]
+public class UnTilledRenderer : TileRenderer {
+	public override void PostRender(Tile tile, object renderData)
+	{
+	}
+
+	public override void RenderInit(Tile tile)
+	{
+		var sr = tile.ThisGameObject.GetComponent<SpriteRenderer>();
+		sr.flipX = Mathf.PerlinNoise(tile.WolrdPos.x, tile.WolrdPos.y) > .5f;
+		sr.flipY = Mathf.PerlinNoise(tile.WolrdPos.y, tile.WolrdPos.x) > .5f;
+	}
+
+	private bool IsEven(float value)
+	{
+		return (value/2f) == (((int)value) / 2f);
+	}
+}
