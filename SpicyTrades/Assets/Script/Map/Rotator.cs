e1e35881b6d8ea3de6d7073afa1e01d2ca4ba73f
Khamraj Rohit <khamraj@luminousvector.com>
2018-05-09 00:50:52 -0400

    Rotating Mill

---
+++ SpicyTrades/Assets/Script/Map/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public float speed;
+
+	private float curAngle;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.localRotation = Quaternion.Euler(0, 0, curAngle);
+		curAngle += Time.deltaTime * speed;
+		if (curAngle >= 360)
+			curAngle -= curAngle;
+	}
+}
---
+++ SpicyTrades/Assets/Script/Scriptable Objects/TileRenderers/MillRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tile Renderer/Mill")]
+public class MillRenderer : TileRenderer
+{
+	public Sprite millBase;
+	public Sprite millBlade;
+	public float speed;
+	public Vector3 bladeOffset;
+	public int sortOrder = 3;
+
+	public override void PostRender(Tile tile, object renderData)
+	{
+	}
+
+	public override void RenderInit(Tile tile)
+	{
+		var body = new GameObject().AddComponent<SpriteRenderer>();
+		body.transform.parent = tile.ThisTransform;
+		body.transform.localPosition = Vector3.zero;
+		body.transform.rotation = Quaternion.Euler(-45, 0, 0);
+		body.sprite = millBase;
+		body.sortingOrder = sortOrder;
+		var blade = new GameObject().AddComponent<SpriteRenderer>();
+		blade.gameObject.AddComponent<Rotator>().speed = speed;
+		blade.transform.parent = body.transform;
+		blade.transform.localPosition = bladeOffset;
+		blade.sprite = millBlade;
+		blade.sortingOrder = sortOrder + 1;
+	}
+}
