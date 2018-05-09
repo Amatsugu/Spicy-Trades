0cfd05d369c62b2686ea48199b0e4540795ff806
Khamraj Rohit <khamraj@luminousvector.com>
2018-05-09 01:36:15 -0400

    misc

--- SpicyTrades/Assets/Script/Scriptable Objects/TileRenderers/ResourceIconRenderer.cs
+++ SpicyTrades/Assets/Script/Scriptable Objects/TileRenderers/ResourceIconRenderer.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Tile Renderer/Resource Icon")]
 public class ResourceIconRenderer : TileRenderer
 {
+	public Sprite renderMask;
+
 	public override void PostRender(Tile tile, object renderData)
 	{
 	}
@@ -13,12 +15,15 @@ public class ResourceIconRenderer : TileRenderer
 	{
 		if (tile.GetType() != typeof(ResourceTile))
 			return;
+		var mask = tile.ThisGameObject.AddComponent<SpriteMask>();
+		mask.sprite = renderMask;
 		var sprite = new GameObject();
 		sprite.transform.parent = tile.ThisTransform;
 		sprite.transform.localPosition = Vector3.zero;
-		sprite.transform.localScale = new Vector3(.8f, .8f, .8f);
+		sprite.transform.localScale = new Vector3(.6f, .6f, .6f);
 		var sr = sprite.AddComponent<SpriteRenderer>();
 		sr.sortingOrder = 3;
+		sr.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
 		sr.sprite = (tile.tileInfo as ResourceTileInfo).icon;
 	}
 }
